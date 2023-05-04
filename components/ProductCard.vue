8a0b4263ac091b5bfb74bceecf6f4175c2d6fbde
nkosi-tauro <nkosilati23@yahoo.com>
2023-05-04 09:51:55 +0200

    added product card

---
+++ components/ProductCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card flex items-center gap-8">
+    <img :src="product.img" :alt="product.title">
+    <div>
+      <p class="text-2xl text-secondary">{{ product.title }}</p>
+      <p class="text-xl text-gray-50">{{ product.description }}</p>
+      <p class="text-lg text-secondary my-3">{{ product.price }} Sliver Coins</p>
+      <button class="btn"><span>Add to Basket</span></button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const { product } = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<style  scoped>
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">All Products</h2>
+
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products.data">
+        <ProductCard :product="product" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+//fetch products from supabase
+const client = useSupabaseClient()
+const { data: products, error} = await useAsyncData('products', async() => client.from('products').select('*').order('id') )
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
